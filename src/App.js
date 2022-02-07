37a2ec6c3bfa9c6589078870cbe30568b93df3ff
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-02-07 11:08:29 +0100

    Add routes to App file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Settings from './pages/Settings';
+import Questions from './pages/Questions';
+import FinalScore from './pages/FinalScore';
 
 function App() {
   return (
     <div className="App">
-      <h1>Quiz App</h1>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Settings />} />
+            <Route path="/questions" element={<Questions />} />
+            <Route path="/score" element={<FinalScore />} />
+          </Routes>
+      </Router>
     </div>
   );
 }
