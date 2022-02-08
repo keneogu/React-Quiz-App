8c540cd172526fd0e0a87ae8e149bf831041e28f
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-02-08 23:27:04 +0100

    Add selected field that takes the selected input with its options

---
+++ src/components/SelectField.js
@@ -0,0 +1,26 @@
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import React, { useState } from 'react'
+
+const SelectField = props => {
+	const { label, options} = props;
+	const [value, setValue] = useState("");
+
+	const handleChange = (e) => {
+		setValue(e.target.value);
+	}
+
+	return (
+		<Box mt={3} width="100%">
+			<FormControl fullWidth>
+				<InputLabel>{label}</InputLabel>
+				<Select value={value} label={label} onChange={handleChange}>
+					{options.map(({ id, name }) => (
+						<MenuItem value={id} key={id}>{name}</MenuItem>
+					))}
+				</Select>
+			</FormControl>
+		</Box>
+	)
+}
+
+export default SelectField
