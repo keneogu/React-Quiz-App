33c31cd2ddc16d15fb4088d8dbde7aaca2d40da9
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-02-08 23:47:56 +0100

    Setting up questions

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import { Box, Typography, Button } from '@mui/material';
+import React from 'react';
+import useAxios from "../common/useAxios";
 
 const Questions = () => {
-	return (
-		<div>
-			Questions
-		</div>
+
+	let apiUrl = `/api.php?amount=10`;
+
+	const { response, loading } = useAxios({ url: apiUrl });
+	
+	 return (
+		<Box>
+			<Typography variant="h4">Question 1</Typography>
+			<Typography variant="h4" mt={5}>Question 2</Typography>
+			<Box mt={2}>
+				<Button variant="contained">Answer 1</Button>
+			</Box>
+			<Box mt={2}>
+				<Button variant="contained">Answer 1</Button>
+			</Box>
+			<Box mt={5}>
+				<Button>Score: 3 /6</Button>
+			</Box>
+		</Box>
 	)
 }
 
