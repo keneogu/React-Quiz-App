7ff1f108ce38e5e060b21ee28772077c0ec07abe
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-02-08 23:30:57 +0100

    Add the options items, circular progress and error message

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,10 +1,56 @@
+import { Box, Button, CircularProgress, Typography } from '@mui/material';
 import React from 'react'
+import SelectField from "../components/SelectField";
+import TextFieldBar from "../components/TextFieldBar";
+import useAxios from "../common/useAxios";
+
 
 const Settings = () => {
+	const {response,error,loading} = useAxios({url: "/api_category.php"})
+	
+	if(loading) {
+		return (
+			<Box mt={20}>
+				<CircularProgress />
+			</Box>
+		)
+	}
+
+	if (error) {
+		return (
+			<Typography variant="h6" mt={20} color="red">
+				Something went wrong
+			</Typography>
+		)
+	}
+
+	const difficultyOptions = [
+		{id: "easy", name: "Easy"},
+		{id: "medium", name: "Medium"},
+		{id: "hard", name: "Hard"},
+	]
+
+	const typeOptions = [
+		{id: "multiple", name: "Multiple Choice"},
+		{id: "boolean", name: "True/False"},
+	]
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
 	return (
-		<div>
-			Settings
-		</div>
+		<form onSubmit={handleSubmit}>
+			<SelectField options={response.trivia_categories} label="Category" />
+			<SelectField options={difficultyOptions} label="Difficulty" />
+			<SelectField options={typeOptions} label="Type" />
+			<TextFieldBar />
+			<Box mt={3} width="100%">
+				<Button fullWidth variant="contained" type="submit">
+					Get Started
+				</Button>
+			</Box>
+		</form>
 	)
 }
 
