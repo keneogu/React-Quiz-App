c5f548166ea6f746360cec136096a6c962eabc0a
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-02-16 13:49:09 +0100

    Add final score which dispatches and updates the final scoere

--- src/pages/FinalScore.js
+++ src/pages/FinalScore.js
@@ -1,11 +1,26 @@
+import { Button, Typography } from '@mui/material'
 import React from 'react'
+import { Box } from "@mui/system";
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
+import { handleScoreChange, handleAmountChange } from '../redux/actions';
 
 const FinalScore = () => {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const { score } = useSelector(state => state);
+	
+	const handleClick = () => {
+		dispatch(handleScoreChange(0));
+		dispatch(handleAmountChange(50));
+		navigate("/")
+	}
 	return (
-		<div>
-			Scores
-		</div>
+		<Box mt={30}>
+			<Typography variant="h3" fontWeight="bold" mb={3}>Final Score {score}</Typography>
+			<Button variant="outline" onClick={handleClick}>back to settings!</Button>
+		</Box>
 	)
 }
 
-export default FinalScore
+export default FinalScore;
